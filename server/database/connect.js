62d58a0278efecfe0fce7dcb9f53b8bdadaa2cb8
Alireza <alirezax69prom@gmail.com>
2023-01-14 07:19:32 +0330

    fix bug

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const { MongoClient } = require('mongodb');
-// const URL = process.env.MONGO_URL;
-const URL = "mongodb://127.0.0.1:27017";
+const URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017";
 
 const client = new MongoClient(URL, {
     connectTimeoutMS: 1000 * 10,
